6d54915a8d164cf86d436730a59ef4b2b64693a0
Andrea <andreafidelibusa@gmail.com>
2018-11-15 09:36:32 +0100

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,49 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+client.close();
+});
+
+
+
+
+
+
+
+
+
+//insert new doc into users (name, age, locations)
+// db.collection('Users').insertOne({
+//   name: 'Andrea',
+//   age: 20,
+//   location: 'San Marino'
+// }, (err, result) => {
+//   if(err) {
+//     return console.log('Unable to insert Users', err);
+//   }
+//   console.log(result.ops[0]._id.getTimestamp());
+//   });
+//   client.close();
+// });
+
+
+
+//insert new doc into Todos (text, completed)
+//   db.collection('Todos').insertOne({
+//     text: 'Something to do',
+//     completed: false
+//   }, (err, result) => {
+//     if(err) {
+//       return console.log('Unable to insert todo', err);
+//     }
+//     console.log(JSON.stringify(result.ops, undefined, 2));
+//   });
+//   client.close();
+// });
